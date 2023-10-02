be4bdc6fb2c69c157e534600a9a1813ea7f47eaa
SarweshKumarTiwari <sarweshtiwari14@gmail.com>
2023-10-02 20:06:03 +0530

    updated auth home

--- client/src/pages/home/AfterAuthHome.tsx
+++ client/src/pages/home/AfterAuthHome.tsx
@@ -1,48 +1,50 @@
+import { Link } from "react-router-dom"
 import Container from "../../components/containers/Container"
 import GridContainer from "../../components/containers/GridContainer"
+import videoTypes from "../../types/VideoTypes"
 
 export default function AfterAuthHome() {
-  const arr = [
+  const arr:videoTypes[] = [
     {
-      title: "Any time ",
+      title: "Any publised_time ",
       channel: "start up",
       image: "https://mir-s3-cdn-cf.behance.net/project_modules/hd/52d24c99322159.5ef08afdcf399.jpg",
-      time: "2 hour ago",
-      videoId:"xyz"
+      publised_time: "2 hour ago",
+      videoId: "xyz"
     },
     {
-      title: "Any time ",
+      title: "Any publised_time ",
       channel: "start up",
       image: "https://mir-s3-cdn-cf.behance.net/project_modules/hd/52d24c99322159.5ef08afdcf399.jpg",
-      time: "2 hour ago",
-      videoId:"xyz"
+      publised_time: "2 hour ago",
+      videoId: "xyz"
     },
     {
-      title: "Any time ",
+      title: "Any publised_time ",
       channel: "start up",
       image: "https://mir-s3-cdn-cf.behance.net/project_modules/hd/52d24c99322159.5ef08afdcf399.jpg",
-      time: "2 hour ago",
-      videoId:"xyz"
+      publised_time: "2 hour ago",
+      videoId: "xyz"
     },
     {
-      title: "Any time ",
+      title: "Any publised_time ",
       channel: "start up",
       image: "https://img.freepik.com/free-vector/professional-suspense-movie-poster_742173-3470.jpg",
-      time: "2 hour ago",
-      videoId:"xyz"
+      publised_time: "2 hour ago",
+      videoId: "xyz"
     },
     {
-      title: "Any time ",
+      title: "Any publised_time ",
       channel: "start up",
       image: "https://img.freepik.com/free-vector/professional-suspense-movie-poster_742173-3470.jpg",
-      time: "2 hour ago",
-      videoId:"xyz"
+      publised_time: "2 hour ago",
+      videoId: "xyz"
     }
   ]
   return (
     <Container>
       <GridContainer>
-        {arr.map((e, i) => <a href="/watch" className="p-1 flex flex-col group space-y-1 " key={i}>
+        {arr.map((e, i) => <Link to="/watch" className="p-1 flex flex-col group space-y-1 " key={i}>
           <img src={e.image} alt="gv" className="rounded-lg w-full h-[10rem] group-hover:blur-sm" />
           <div className="p-4">
             <h3 className="text-md text-bold">{e.title}</h3>
@@ -53,11 +55,11 @@ export default function AfterAuthHome() {
                   <path d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0zm-3.97-3.03a.75.75 0 0 0-1.08.022L7.477 9.417 5.384 7.323a.75.75 0 0 0-1.06 1.06L6.97 11.03a.75.75 0 0 0 1.079-.02l3.992-4.99a.75.75 0 0 0-.01-1.05z" />
                 </svg>
               </div>
-              <p>{e.time}</p>
+              <p>{e.publised_time}</p>
             </div>
           </div>
-        </a>)}
+        </Link>)}
       </GridContainer>
-      </Container>
+    </Container>
   )
 }
