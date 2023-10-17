0cacfe7bd0ab85c2eb385c70ae1b59bf94dc1966
sarweshkumartiwari <sarweshtiwari14@gmail.com>
2023-10-17 13:36:26 +0530

    removed extra navbar

--- client/src/pages/Player.tsx
+++ client/src/pages/Player.tsx
@@ -1,11 +1,9 @@
-import NavBar from "../components/NavBar";
 import VideoComments from "./player/VideoComments";
 import VideoPlayer from "./player/VideoPlayer";
 
 export default function Player() {
   return (
     <>
-    <NavBar/>
     <div className="md:mx-2 flex flex-col md:flex md:flex-row">
       <VideoPlayer/>
       <VideoComments/>
