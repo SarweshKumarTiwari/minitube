d5cda6a1e53a6fefd34685f27b0402e45aba2d62
sarweshkumartiwari <sarweshtiwari14@gmail.com>
2023-11-03 22:51:03 +0530

    updated token utilities

--- server/utils/tokenHandler.ts
+++ server/utils/tokenHandler.ts
@@ -1,33 +1,15 @@
-import jwt,{ JwtPayload, SignOptions,TokenExpiredError,JsonWebTokenError,} from "jsonwebtoken";
+import jwt,{ JwtPayload, SignOptions} from "jsonwebtoken";
 import dotenv from "dotenv";
 dotenv.config();
 class token{
 
-    generateToken(payload:JwtPayload,timelimit?:SignOptions):string{
-        try {
-            return jwt.sign(payload,process.env.ACCESS_TOKEN as string,timelimit)
-        } catch (error) {
-            if (error instanceof JsonWebTokenError) {
-                return error.message
-            }
-            return "JSON web token error"
-        }
+    generateToken(payload:JwtPayload,secret:string,timelimit?:SignOptions):string{
+            return jwt.sign(payload,secret,timelimit)
+        
     }
 
-    verifyToken(token:string){
-        try {
-            return jwt.verify(token,process.env.ACCESS_TOKEN as string);
-        } catch (error) {
-            if (error instanceof TokenExpiredError) {
-                return error.message;
-            }
-            else if(error instanceof JsonWebTokenError){
-                return error.message
-            }
-            else{
-                return null;
-            }
-        }     
+    verifyToken(token:string,secret:string){
+        return jwt.verify(token,secret);
     }
 
 }
