eeafc8a9b1f975048ff9369949e61dbabee6a09e
sarweshkumartiwari <sarweshtiwari14@gmail.com>
2023-11-05 23:35:37 +0530

    added async await in connection db

--- server/config/connectDB.ts
+++ server/config/connectDB.ts
@@ -3,11 +3,12 @@ import dotenv from "dotenv";
 
 dotenv.config();
 const uri=process.env.DB_CONNECTION_URL||"";
-export default ()=>{
+export default async()=>{
     try {
-        mongoose.connect(uri,{dbName:"Minitube"});
-        console.log("database connected successfully");
+        const connectionStatus=await mongoose.connect(uri,{dbName:"Minitube"});
+        console.log("Database is connected at ",connectionStatus.connection.host);
     } catch (error) {
         console.log(error);
+        process.exit(1);
     }
 }
