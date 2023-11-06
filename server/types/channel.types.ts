dc89f351be91b7455eb337f42979b7a65388e5dc
sarweshkumartiwari <sarweshtiwari14@gmail.com>
2023-11-06 23:49:48 +0530

    added video and channel types

---
+++ server/types/channel.types.ts
@@ -0,0 +1,6 @@
+export type channelType={
+    c_name:string
+    c_videos?:number
+    subscribers?:number
+    u_id:string
+}
---
+++ server/types/video.types.ts
@@ -0,0 +1,18 @@
+export type videoTypes={
+    v_title:string
+    v_categ:string
+    v_desc?:string
+    v_url:string
+    v_duration:string
+    allow_comments?:boolean
+    c_id:string
+}
+
+export type videoMutate={
+    v_title?:string
+    v_categ?:string
+    v_desc?:string
+    v_url?:string
+    v_duration?:string
+    allow_comments?:boolean
+}
