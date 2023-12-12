f580d45e49045c7df54c5c914faf9109a20a6937
sarweshkumartiwari <sarweshtiwari14@gmail.com>
2023-12-12 13:53:36 +0530

    removed error bugs

--- server/models/history/history.models.ts
+++ server/models/history/history.models.ts
@@ -1,27 +1,31 @@
 import { historyType } from "../../types/history.types";
+import { AppError } from "../../utils/errorHander";
 import historyEntity from "./history.entity"
-import videoEntity from "../videos/videos.entity"
 
 class HistoryModel{
     async insertHistory(history:historyType){
-        try {
             return await new historyEntity(history).save();
-        } catch (error) {
-            throw new Error(error)
-        }
     }
 
     async getAllHistoryOfUser(userId:string){
-        const result=await historyEntity.find({u_id:userId},{v_id:1,_id:1});
-        if (!result) {
-            throw new Error("There is no history");
-        }
-        const final=await videoEntity.find({_id:{$all:result.map(e=>e.v_id)}});
-        return final
+        const result=await historyEntity.find({u_id:userId});
+        return result;
     }
 
-    async name(params:any){
-        
+    async delete_A_History(h_id:string){
+        const result=await historyEntity.findByIdAndDelete(h_id);
+        if (!result) {
+            throw new AppError("please check id",400);
+        }
+        return result;
+    }
+
+    async clearHistory(u_id:string){
+        const result=await historyEntity.deleteMany({u_id:u_id});
+        if (!result) {
+            throw new AppError("please check user id",404);
+        }
+        return result;
     }
 }
 
