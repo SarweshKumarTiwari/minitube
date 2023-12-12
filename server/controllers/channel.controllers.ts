45d95e9673c264ab887f3a4b1872a60f11394944
sarweshkumartiwari <sarweshtiwari14@gmail.com>
2023-12-12 13:50:19 +0530

    introduced async handler by removing try catch

--- server/controllers/channel.controllers.ts
+++ server/controllers/channel.controllers.ts
@@ -1,20 +1,16 @@
 import { Request,Response } from "express";
 import channelsModels from "../models/channels/channels.models";
+import asyncHandler from "../utils/asyncHandler";
+import { AppError } from "../utils/errorHander";
 
 class ChannelController{
-    async newChannel(req:Request,res:Response){
-        try {
-            const request=req.body;
-            if (!(request.c_name&&request.u_id)) {
-                return res.status(400).json({error:"Some fields are empty"})
-            }
-            const result=await channelsModels.addChannel(request);
-            return res.status(200).json({success:result})
-        } catch (error) {
-            console.log(error);
-            return res.status(500).json({error:error});
+    public newChannel = asyncHandler(async function (req:Request,res:Response){
+        const request=req.body;
+        if (!(request.c_name&&request.u_id)) {
+            throw new AppError("Some fields are empty",400);
         }
-
-    }
+        const result=await channelsModels.addChannel(request);
+        return res.status(200).json({success:result})
+    })
 }
 export default new ChannelController();
