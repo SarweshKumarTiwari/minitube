267ecb2c67a10ee37f2c2a822a744a5e28cc3bb6
sarweshkumartiwari <sarweshtiwari14@gmail.com>
2023-12-21 23:33:03 +0530

    from div to link

--- client/src/components/navbar/SmallScreenNav.tsx
+++ client/src/components/navbar/SmallScreenNav.tsx
@@ -65,11 +65,11 @@ export default function SmallScreenNav(props: { isUser: boolean,userName:string}
                     <h2 className="text-2xl text-gray-800">MiniTube</h2>
                 </a>
                 <div className="flex mr-2 space-x-[12px]">
-                    <div className="p-2">
+                    <a href="/search" className="p-2">
                         <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" className="fill-gray-400 stroke-2" viewBox="0 0 16 16">
                             <path d="M11.742 10.344a6.5 6.5 0 1 0-1.397 1.398h-.001c.03.04.062.078.098.115l3.85 3.85a1 1 0 0 0 1.415-1.414l-3.85-3.85a1.007 1.007 0 0 0-.115-.1zM12 6.5a5.5 5.5 0 1 1-11 0 5.5 5.5 0 0 1 11 0z" />
                         </svg>
-                    </div>
+                    </a>
                     <div className="p-2 flex items-center space-x-[8px] rounded-xl border border-greenc bg-greenc text-xl text-white" onClick={() => setdropdown(!dropdown)}>
                         {props.isUser ?
                             <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" className="fill-white" viewBox="0 0 16 16">
