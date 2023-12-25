c0c8a0207cce24728e4df89ed3efc935d77f720b
sarweshkumartiwari <sarweshtiwari14@gmail.com>
2023-12-25 22:07:52 +0530

    added <Navlink>

--- client/src/components/navbar/SmallScreenNav.tsx
+++ client/src/components/navbar/SmallScreenNav.tsx
@@ -1,5 +1,6 @@
 import { useState } from "react";
 import logo1 from "../../assets/favicon-32x32.png"
+import { NavLink } from "react-router-dom";
 
 function BeforeAuth() {
     return (
@@ -8,14 +9,14 @@ function BeforeAuth() {
                 <p>!Hey would you like to join us.</p>
                 <p>we are providing ads free streaming and sharing videos. </p>
             </li>
-            <a href="/signup" className="p-2 w-3/4 bg-greenc text-xl text-white rounded-xl flex justify-center items-center space-x-[4px]">
+            <NavLink to="/signup" className="p-2 w-3/4 bg-greenc text-xl text-white rounded-xl flex justify-center items-center space-x-[4px]">
                 <p >Get Started</p>
                 <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" className="fill-white" viewBox="0 0 16 16">
                     <path fillRule="evenodd" d="M1 8a.5.5 0 0 1 .5-.5h11.793l-3.147-3.146a.5.5 0 0 1 .708-.708l4 4a.5.5 0 0 1 0 .708l-4 4a.5.5 0 0 1-.708-.708L13.293 8.5H1.5A.5.5 0 0 1 1 8z" />
                 </svg>
-            </a>
+            </NavLink>
             <li className="p-2 text-xl text-gray-600">
-                <a href="/signin">SignIn</a>
+                <NavLink to="/signin">SignIn</NavLink>
             </li>
         </ul>
     )
@@ -24,34 +25,34 @@ function BeforeAuth() {
 function AfterAuth() {
     return (
         <div className="flex flex-col space-y-2  items-center w-full">
-            <a href="/history" className="flex p-2 space-x-[4px] text-gray-600 text-lg items-center">
+            <NavLink to="/history" className="flex p-2 space-x-[4px] text-gray-600 text-lg items-center">
                 <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" className="fill-gray-600" viewBox="0 0 16 16">
                     <path d="M8.515 1.019A7 7 0 0 0 8 1V0a8 8 0 0 1 .589.022l-.074.997zm2.004.45a7.003 7.003 0 0 0-.985-.299l.219-.976c.383.086.76.2 1.126.342l-.36.933zm1.37.71a7.01 7.01 0 0 0-.439-.27l.493-.87a8.025 8.025 0 0 1 .979.654l-.615.789a6.996 6.996 0 0 0-.418-.302zm1.834 1.79a6.99 6.99 0 0 0-.653-.796l.724-.69c.27.285.52.59.747.91l-.818.576zm.744 1.352a7.08 7.08 0 0 0-.214-.468l.893-.45a7.976 7.976 0 0 1 .45 1.088l-.95.313a7.023 7.023 0 0 0-.179-.483zm.53 2.507a6.991 6.991 0 0 0-.1-1.025l.985-.17c.067.386.106.778.116 1.17l-1 .025zm-.131 1.538c.033-.17.06-.339.081-.51l.993.123a7.957 7.957 0 0 1-.23 1.155l-.964-.267c.046-.165.086-.332.12-.501zm-.952 2.379c.184-.29.346-.594.486-.908l.914.405c-.16.36-.345.706-.555 1.038l-.845-.535zm-.964 1.205c.122-.122.239-.248.35-.378l.758.653a8.073 8.073 0 0 1-.401.432l-.707-.707z" />
                     <path d="M8 1a7 7 0 1 0 4.95 11.95l.707.707A8.001 8.001 0 1 1 8 0v1z" />
                     <path d="M7.5 3a.5.5 0 0 1 .5.5v5.21l3.248 1.856a.5.5 0 0 1-.496.868l-3.5-2A.5.5 0 0 1 7 9V3.5a.5.5 0 0 1 .5-.5z" />
                 </svg>
                 <p>Watch History</p>
-            </a>
-            <a href="/myvideos" className="flex p-2 space-x-[4px] text-gray-600 text-lg items-center" >
+            </NavLink>
+            <NavLink to="/myvideos" className="flex p-2 space-x-[4px] text-gray-600 text-lg items-center" >
                 <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" className="fill-gray-600" viewBox="0 0 16 16">
                     <path fillRule="evenodd" d="M0 5a2 2 0 0 1 2-2h7.5a2 2 0 0 1 1.983 1.738l3.11-1.382A1 1 0 0 1 16 4.269v7.462a1 1 0 0 1-1.406.913l-3.111-1.382A2 2 0 0 1 9.5 13H2a2 2 0 0 1-2-2V5zm11.5 5.175 3.5 1.556V4.269l-3.5 1.556v4.35zM2 4a1 1 0 0 0-1 1v6a1 1 0 0 0 1 1h7.5a1 1 0 0 0 1-1V5a1 1 0 0 0-1-1H2z" />
                 </svg>
                 <p>My Videos</p>
-            </a>
-            <a href="/" className="flex p-2 space-x-[4px] text-gray-600 text-lg items-center" >
+            </NavLink>
+            <NavLink to="/" className="flex p-2 space-x-[4px] text-gray-600 text-lg items-center" >
                 <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" className="fill-gray-600" viewBox="0 0 16 16">
                     <path d="M8 4.754a3.246 3.246 0 1 0 0 6.492 3.246 3.246 0 0 0 0-6.492zM5.754 8a2.246 2.246 0 1 1 4.492 0 2.246 2.246 0 0 1-4.492 0z" />
                     <path d="M9.796 1.343c-.527-1.79-3.065-1.79-3.592 0l-.094.319a.873.873 0 0 1-1.255.52l-.292-.16c-1.64-.892-3.433.902-2.54 2.541l.159.292a.873.873 0 0 1-.52 1.255l-.319.094c-1.79.527-1.79 3.065 0 3.592l.319.094a.873.873 0 0 1 .52 1.255l-.16.292c-.892 1.64.901 3.434 2.541 2.54l.292-.159a.873.873 0 0 1 1.255.52l.094.319c.527 1.79 3.065 1.79 3.592 0l.094-.319a.873.873 0 0 1 1.255-.52l.292.16c1.64.893 3.434-.902 2.54-2.541l-.159-.292a.873.873 0 0 1 .52-1.255l.319-.094c1.79-.527 1.79-3.065 0-3.592l-.319-.094a.873.873 0 0 1-.52-1.255l.16-.292c.893-1.64-.902-3.433-2.541-2.54l-.292.159a.873.873 0 0 1-1.255-.52l-.094-.319zm-2.633.283c.246-.835 1.428-.835 1.674 0l.094.319a1.873 1.873 0 0 0 2.693 1.115l.291-.16c.764-.415 1.6.42 1.184 1.185l-.159.292a1.873 1.873 0 0 0 1.116 2.692l.318.094c.835.246.835 1.428 0 1.674l-.319.094a1.873 1.873 0 0 0-1.115 2.693l.16.291c.415.764-.42 1.6-1.185 1.184l-.291-.159a1.873 1.873 0 0 0-2.693 1.116l-.094.318c-.246.835-1.428.835-1.674 0l-.094-.319a1.873 1.873 0 0 0-2.692-1.115l-.292.16c-.764.415-1.6-.42-1.184-1.185l.159-.291A1.873 1.873 0 0 0 1.945 8.93l-.319-.094c-.835-.246-.835-1.428 0-1.674l.319-.094A1.873 1.873 0 0 0 3.06 4.377l-.16-.292c-.415-.764.42-1.6 1.185-1.184l.292.159a1.873 1.873 0 0 0 2.692-1.115l.094-.319z" />
                 </svg>
                 <p>Pofile Settings</p>
-            </a>
-            <a href="/" className="flex p-2 space-x-[4px] text-gray-600 text-lg items-center" >
+            </NavLink>
+            <NavLink to="/" className="flex p-2 space-x-[4px] text-gray-600 text-lg items-center" >
                 <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" className="fill-rose-400" viewBox="0 0 16 16">
                     <path d="M8.5 10c-.276 0-.5-.448-.5-1s.224-1 .5-1 .5.448.5 1-.224 1-.5 1z" />
                     <path d="M10.828.122A.5.5 0 0 1 11 .5V1h.5A1.5 1.5 0 0 1 13 2.5V15h1.5a.5.5 0 0 1 0 1h-13a.5.5 0 0 1 0-1H3V1.5a.5.5 0 0 1 .43-.495l7-1a.5.5 0 0 1 .398.117zM11.5 2H11v13h1V2.5a.5.5 0 0 0-.5-.5zM4 1.934V15h6V1.077l-6 .857z" />
                 </svg>
                 <p>Sign Out</p>
-            </a>
+            </NavLink>
         </div>
     )
 }
@@ -60,16 +61,16 @@ export default function SmallScreenNav(props: { isUser: boolean,userName:string}
     return (
         <div className="md:hidden md:invisible z-10 sticky top-0 bg-[#ffffff]">
             <div className="p-2 m-2 flex justify-between items-center ">
-                <a href="/" className="flex items-center space-x-[6px]">
+                <NavLink to="/" className="flex items-center space-x-[6px]">
                     <img src={logo1} alt="logo" width={32} height={32} />
                     <h2 className="text-2xl text-gray-800">MiniTube</h2>
-                </a>
+                </NavLink>
                 <div className="flex mr-2 space-x-[12px]">
-                    <a href="/search" className="p-2">
+                    <NavLink to="/search" className="p-2">
                         <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" className="fill-gray-400 stroke-2" viewBox="0 0 16 16">
                             <path d="M11.742 10.344a6.5 6.5 0 1 0-1.397 1.398h-.001c.03.04.062.078.098.115l3.85 3.85a1 1 0 0 0 1.415-1.414l-3.85-3.85a1.007 1.007 0 0 0-.115-.1zM12 6.5a5.5 5.5 0 1 1-11 0 5.5 5.5 0 0 1 11 0z" />
                         </svg>
-                    </a>
+                    </NavLink>
                     <div className="p-2 flex items-center space-x-[8px] rounded-xl border border-greenc bg-greenc text-xl text-white" onClick={() => setdropdown(!dropdown)}>
                         {props.isUser ?
                             <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" className="fill-white" viewBox="0 0 16 16">
