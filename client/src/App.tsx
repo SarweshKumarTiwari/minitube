ba50b2e19e9ee0e0c9197bf6773edd133d8f380e
sarweshkumartiwari <sarweshtiwari14@gmail.com>
2024-01-02 22:55:01 +0530

    added react-query

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,10 +1,14 @@
 import WebRoutes from "./routes/WebRoutes";
 import AuthContextProvider from "./contexts/AuthContext/AuthContextProvider";
+import { QueryClient, QueryClientProvider } from "react-query";
+const query=new QueryClient();
 function App() {
   return (
+    <QueryClientProvider client={query}>
     <AuthContextProvider>
       <WebRoutes />
     </AuthContextProvider>
+    </QueryClientProvider>
   );
 }
 
