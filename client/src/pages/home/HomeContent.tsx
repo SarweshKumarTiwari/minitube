a7bd799709c9edf2bed382e2a4eaaebbc15d6a8a
sarweshkumartiwari <sarweshtiwari14@gmail.com>
2024-01-02 22:53:41 +0530

    improved code

--- client/src/pages/home/HomeContent.tsx
+++ client/src/pages/home/HomeContent.tsx
@@ -1,4 +1,3 @@
-import { useRef } from "react";
 import { Link } from "react-router-dom"
 import Container from "../../components/containers/Container"
 import GridContainer from "../../components/containers/GridContainer"
@@ -7,38 +6,38 @@ import VideoGridCard from "../../components/cards/VideoGridCard"
 import InfiniteScrollHook from "../../hooks/InfiniteScrollHook";
 
 export default function HomeContent() {
-  const arr:videoTypes[] = [
+  const arr: videoTypes[] = [
     {
       v_title: "Any publised_time ",
-      channel: {_id:"fgh",c_name:"sample"},
+      channel: { _id: "fgh", c_name: "sample" },
       v_cover: "https://mir-s3-cdn-cf.behance.net/project_modules/hd/52d24c99322159.5ef08afdcf399.jpg",
       publised_time: "2 hour ago",
       videoId: "xyz"
     },
     {
       v_title: "Any publised_time ",
-      channel: {_id:"fgh",c_name:"sample"},
+      channel: { _id: "fgh", c_name: "sample" },
       v_cover: "https://mir-s3-cdn-cf.behance.net/project_modules/hd/52d24c99322159.5ef08afdcf399.jpg",
       publised_time: "2 hour ago",
       videoId: "xyz"
     },
     {
       v_title: "Any publised_time ",
-      channel: {_id:"fgh",c_name:"sample"},
+      channel: { _id: "fgh", c_name: "sample" },
       v_cover: "https://mir-s3-cdn-cf.behance.net/project_modules/hd/52d24c99322159.5ef08afdcf399.jpg",
       publised_time: "2 hour ago",
       videoId: "xyz"
     },
     {
       v_title: "Any publised_time ",
-      channel: {_id:"fgh",c_name:"sample"},
+      channel: { _id: "fgh", c_name: "sample" },
       v_cover: "https://img.freepik.com/free-vector/professional-suspense-movie-poster_742173-3470.jpg",
       publised_time: "2 hour ago",
       videoId: "xyz"
     },
     {
       v_title: "Any publised_time ",
-      channel: {_id:"fgh",c_name:"sample"},
+      channel: { _id: "fgh", c_name: "sample" },
       v_cover: "https://img.freepik.com/free-vector/professional-suspense-movie-poster_742173-3470.jpg",
       publised_time: "2 hour ago",
       videoId: "xyz"
@@ -46,7 +45,7 @@ export default function HomeContent() {
     ,
     {
       v_title: "Any publised_time ",
-      channel: {_id:"fgh",c_name:"sample"},
+      channel: { _id: "fgh", c_name: "sample" },
       v_cover: "https://img.freepik.com/free-vector/professional-suspense-movie-poster_742173-3470.jpg",
       publised_time: "2 hour ago",
       videoId: "xyz"
@@ -54,7 +53,7 @@ export default function HomeContent() {
     ,
     {
       v_title: "Any publised_time ",
-      channel: {_id:"fgh",c_name:"sample"},
+      channel: { _id: "fgh", c_name: "sample" },
       v_cover: "https://img.freepik.com/free-vector/professional-suspense-movie-poster_742173-3470.jpg",
       publised_time: "2 hour ago",
       videoId: "xyz"
@@ -62,20 +61,23 @@ export default function HomeContent() {
     ,
     {
       v_title: "Any publised_time ",
-      channel: {_id:"fgh",c_name:"sample"},
+      channel: { _id: "fgh", c_name: "sample" },
       v_cover: "https://img.freepik.com/free-vector/professional-suspense-movie-poster_742173-3470.jpg",
       publised_time: "2 hour ago",
       videoId: "xyz"
     }
   ]
-  const lastElementRef=useRef<HTMLDivElement|null>(null);
-  InfiniteScrollHook({lastElementRef:lastElementRef.current})
+  const {divReference}= InfiniteScrollHook()
   return (
     <Container>
       <GridContainer>
-        {arr.map((e, i) => <Link to="/watch" state={e}  key={i}>
-          <VideoGridCard data={e} point={arr.length-1===i?lastElementRef:null}/>
-        </Link>)}
+        {arr.map((e, i) => {
+          return (
+            <Link to="/watch" state={e} key={i}>
+              <VideoGridCard data={e} point={arr.length - 1 === i ? divReference : null} />
+            </Link>
+            )
+        })}
       </GridContainer>
     </Container>
   )
